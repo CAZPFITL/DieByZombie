b4714d58aaec7730e0d30d312a80818890262c76
caz <cazpfitl@gmail.com>
2022-08-15 10:20:49 -0500

    feat: v1.0.1

--- assets/js/engine/utils/components/Physics.js
+++ assets/js/engine/utils/components/Physics.js
@@ -1,14 +1,15 @@
 export default class Physics {
     constructor(app, callback = (fn) => fn()) {
         this.app = app;
-        this.stopRange = 0.05 * this.app.gameSpeed;
-        callback(()=> {
+        this.stopRange = 0.08;
+        callback(() => {
             this.app.log.registerEvent(
                 `New Physics Created`,
                 `\x1b[32;1m| \x1b[0mNew \x1b[32;1mPhysics\x1b[0m Created`
             );
         });
     }
+
     /**
      * Class methods
      */
@@ -32,19 +33,17 @@ export default class Physics {
     // TODO remove entity and create methods for calculations
     move(entity) {
         // limit the speed to maxSpeed
-        (entity.speed > entity.maxSpeed) && (entity.speed = entity.maxSpeed);
-        (entity.speed < -entity.maxSpeed) && (entity.speed = -entity.maxSpeed);
-
-        // absolute stop the entity
-        (entity.speed > -this.stopRange) &&
-            (entity.speed < this.stopRange) &&
-                (entity.speed = 0);
+        if (entity.speed > entity.maxSpeed) entity.speed = entity.maxSpeed;
+        if (entity.speed < -entity.maxSpeed) entity.speed = -entity.maxSpeed;
 
         // add friction and absolute repose in lower ranges
-        (entity.speed > 0) && (entity.speed -= entity.friction);
-        (entity.speed < 0) && (entity.speed += entity.friction);
+        if (entity.speed > 0) entity.speed -= entity.friction;
+        if (entity.speed < 0) entity.speed += entity.friction;
 
-        // this works under the unit circle logic using sin or cos multiplied by speed to get the translation
+        // absolute stop the entity
+        if ((entity.speed > -this.stopRange) && (entity.speed < this.stopRange)) entity.speed = 0;
+
+        // World Limits
         this.worldLimits({
             x: Math.sin(entity.angle) * entity.speed,
             y: Math.cos(entity.angle) * entity.speed
@@ -53,15 +52,15 @@ export default class Physics {
 
     worldLimits({x, y}, entity) {
         const coords = entity.coords;
-        // Limit Movement
-        (
-            !this.app.gui.get.polysIntersect(entity.polygons, this.app.game.level.boundTargets.polygons.slice(0, 4)) &&
-            !this.app.gui.get.polysIntersect(entity.polygons, this.app.game.level.boundTargets.polygons.slice(5, 8))
-        ) ? (coords.x -= x) : (coords.x -= coords.x > 0 ? 0.1 : -0.1);
-        (
-            !this.app.gui.get.polysIntersect(entity.polygons, this.app.game.level.boundTargets.polygons.slice(8, 11)) &&
-            !this.app.gui.get.polysIntersect(entity.polygons, this.app.game.level.boundTargets.polygons.slice(12, 15))
-        ) ? (coords.y -= y) : (coords.y -= coords.y > 0 ? 0.1 : -0.1);
+        const borders = this.app.game.level.boundTargets.polygons;
+        // Limit Horizontal Movement
+        (!this.app.gui.get.polysIntersect(entity.polygons, borders.slice(0, 4)) &&
+            !this.app.gui.get.polysIntersect(entity.polygons, borders.slice(5, 8)))
+            ? (coords.x -= x) : (coords.x -= coords.x > 0 ? 0.1 : -0.1);
+        // Limit Vertical Movement
+        (!this.app.gui.get.polysIntersect(entity.polygons, borders.slice(8, 11)) &&
+            !this.app.gui.get.polysIntersect(entity.polygons, borders.slice(12, 15)))
+            ? (coords.y -= y) : (coords.y -= coords.y > 0 ? 0.1 : -0.1);
     }
 
     isInBound(entity) {
